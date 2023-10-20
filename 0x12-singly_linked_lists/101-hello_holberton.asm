3e8d9ee4557b7f71c99310caed37694b4e056054
Elormie <Princeagbebo@gmail.com>
2023-10-20 00:06:29 -0700

    Singly Linked Lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,11 @@
-.global main
-.extern printf
+section .data
+    format db "Hello, Holberton", 0
+
+section .text
+    global main
+    extern printf
 
 main:
-    mov    edi, format
-    xor    eax, eax
-    call   printf
-    mov    eax, 0
+    mov rdi, format
+    call printf
     ret
-
-format:
-    db     "Hello, Holberton", 0
