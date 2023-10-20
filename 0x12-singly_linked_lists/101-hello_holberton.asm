6e105bd32b074d372a8272e21ef7b3140787e96e
Elormie <Princeagbebo@gmail.com>
2023-10-20 00:12:59 -0700

    Singly Linked Lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,13 @@
 section .data
-    format db "Hello, Holberton", 0
+    format db "Hello, Holberton", 10, 0  ; 10 represents a newline character
 
 section .text
     global main
     extern printf
 
 main:
+    sub rsp, 8    ; align the stack
     mov rdi, format
     call printf
+    add rsp, 8    ; restore the stack
     ret
